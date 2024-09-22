09dfd2c2ee0ef9e826a655870933efcf0ffbd2b6
ShiDai <shidai567567@gmail.com>
2024-09-23 00:57:38 +0800

    更新背景音乐按钮

--- js/music.js
+++ js/music.js
@@ -1,7 +1,9 @@
 var bg_music_on_obj = document.getElementById("bg_music_on")
 var bg_music_off_obj = document.getElementById("bg_music_off")
 bg_music_on_obj.style.display = "inline"
+bg_music_on_obj.style.padding = "10px"
 bg_music_off_obj.style.display = "none"
+bg_music_off_obj.style.padding = "10px"
 
 //bg_music_on_obj.style.display = "none"
 //bg_music_off_obj.style.display = "inline"
@@ -9,20 +11,29 @@ function img_rotate() {
     var img_on = document.getElementById('bg_music_on');
     current = (current + 1) % 360;
     img_on.style.transform = 'rotate(' + current + 'deg)';
-    setTimeout(img_rotate, 25);
+    setTimeout(img_rotate, 50);
 }
+//图案位置复原
+var current = 0;
 
+
+
+//音乐开启
 function bg_music_on() {
     bg_music.play();
+    img_rotate();
     bg_music_off_obj.style.display = "none";
     bg_music_on_obj.style.display = "inline";
-    console.log("music off");
+    console.log("music on");
 }
+
+
+//音乐关闭
 function bg_music_off() {
     bg_music.pause();
     bg_music_off_obj.style.display = "inline";
     bg_music_on_obj.style.display = "none";
-    console.log("music on");
+    console.log("music off");
+    current = 0;
 }
-var current = 0;
 
