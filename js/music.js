afc8fa1800c42aa864516177b5d3c49cae7e684f
ShiDai <shidai567567@gmail.com>
2024-09-22 23:57:47 +0800

    修复导航栏和团队介绍的bug

--- js/music.js
+++ js/music.js
@@ -1,10 +1,10 @@
 var bg_music_on_obj = document.getElementById("bg_music_on")
 var bg_music_off_obj = document.getElementById("bg_music_off")
-//bg_music_on_obj.style.display = "inline"
-//bg_music_off_obj.style.display = "none"
+bg_music_on_obj.style.display = "inline"
+bg_music_off_obj.style.display = "none"
 
-bg_music_on_obj.style.display = "none"
-bg_music_off_obj.style.display = "inline"
+//bg_music_on_obj.style.display = "none"
+//bg_music_off_obj.style.display = "inline"
 function img_rotate() {
     var img_on = document.getElementById('bg_music_on');
     current = (current + 1) % 360;
@@ -14,14 +14,14 @@ function img_rotate() {
 
 function bg_music_on() {
     bg_music.play();
-    bg_music_off_obj.style.display = "inline";
-    bg_music_on_obj.style.display = "none";
+    bg_music_off_obj.style.display = "none";
+    bg_music_on_obj.style.display = "inline";
     console.log("music off");
 }
 function bg_music_off() {
     bg_music.pause();
-    bg_music_off_obj.style.display = "none";
-    bg_music_on_obj.style.display = "inline";
+    bg_music_off_obj.style.display = "inline";
+    bg_music_on_obj.style.display = "none";
     console.log("music on");
 }
 var current = 0;
--- js/time.js
+++ js/time.js
@@ -6,12 +6,12 @@ var minute = 0;
 var second = 0;
 
 var today = new Date();//定义日期对象
-//var yyyy = parseInt(today.getFullYear());//通过日期对象的getFullYear()方法返回年    
+//var yyyy = parseInt(today.getFullYear());//通过日期对象的getFullYear()方法返回年
 //var MM = parseInt(today.getMonth() + 1);//通过日期对象的getMonth()方法返回月
-//var dd = parseInt(today.getDate());//通过日期对象的getDate()方法返回日 
-var hh = parseInt(today.getHours());//通过日期对象的getHours方法返回小时   
-var mm = parseInt(today.getMinutes());//通过日期对象的getMinutes方法返回分钟   
-var ss = parseInt(today.getSeconds());//通过日期对象的getSeconds方法返回秒   
+//var dd = parseInt(today.getDate());//通过日期对象的getDate()方法返回日
+var hh = parseInt(today.getHours());//通过日期对象的getHours方法返回小时
+var mm = parseInt(today.getMinutes());//通过日期对象的getMinutes方法返回分钟
+var ss = parseInt(today.getSeconds());//通过日期对象的getSeconds方法返回秒
 // 如果分钟或小时的值小于10，则在其值前加0，比如如果时间是下午3点20分9秒的话，则显示15：20：09
 
 var sum_day = parseInt((new Date().getTime() - new Date("2020-1-28").getTime()) / 86400000);
